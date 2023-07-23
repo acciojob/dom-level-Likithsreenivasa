5d603158d2164147d5758e5f88fb54c7e10dc1e1
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-23 18:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+        const targetElement=document.getElementById("level");
+        let domLevel=0;
+        let currentElement=targetElement;
+        while(currentElement)
+        {
+            domLevel++;
+            currentElement=currentElement.parentElement;
+        }
+    alert(`The level of the element is ${domLevel}`);
+    
